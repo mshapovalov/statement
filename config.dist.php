39ae2b5d24ca92cdf0e5881c868d39ccf5c0981f
Mike Shapovalov <smike.mbx@gmail.com>
2020-11-30 09:10:55 +0100

    config

--- config.dist.php
+++ config.dist.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 return [
-    'author' => '',
-    'token' => '',
-    'company' => '',
+    'author' => '', //you github username
+    'token' => '', //github access token https://github.com/settings/tokens
+    'company' => 'ifxtech', //company name
+    'secret' => 'secret', //secret that you pass in url
     'repositories' => [
         'MassPay'
     ]
